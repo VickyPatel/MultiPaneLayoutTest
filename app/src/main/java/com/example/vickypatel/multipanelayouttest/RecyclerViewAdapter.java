e4bb0f76cc2a8f953299d5972ae085a55cfa92b5
vicky patel <vicky.patel@hotmail.com>
2017-02-13 20:07:42 -0500

    two pane done

--- app/src/main/java/com/example/vickypatel/multipanelayouttest/RecyclerViewAdapter.java
+++ app/src/main/java/com/example/vickypatel/multipanelayouttest/RecyclerViewAdapter.java
@@ -2,6 +2,8 @@ package com.example.vickypatel.multipanelayouttest;
 
 import android.content.Context;
 import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -60,13 +62,22 @@ public class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewAdapte
 
                 @Override
                 public void onClick(View v) {
-                    Toast.makeText(mContext, mItems.get(getAdapterPosition()), Toast.LENGTH_LONG).show();
 
-                    Context context = v.getContext();
-                    Intent intent = new Intent(context, DetailActivity.class);
-                    intent.putExtra(DetailFragment.DATA, mItems.get(getAdapterPosition()));
-
-                    context.startActivity(intent);
+                    if(mTwoPane){
+                        Bundle arguments = new Bundle();
+                        arguments.putString(DetailFragment.DATA,
+                                mItems.get(getAdapterPosition()));
+                        DetailFragment fragment = new DetailFragment();
+                        fragment.setArguments(arguments);
+                        ((FragmentActivity)mContext).getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.item_detail_container, fragment)
+                                .commit();
+                    }else{
+                        Context context = v.getContext();
+                        Intent intent = new Intent(context, DetailActivity.class);
+                        intent.putExtra(DetailFragment.DATA, mItems.get(getAdapterPosition()));
+                        context.startActivity(intent);
+                    }
 
                 }
             });
