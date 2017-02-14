14314ecdf9787428745fd5b1175ec8a7a1a9f8a4
vicky patel <vicky.patel@hotmail.com>
2017-02-13 20:13:49 -0500

    default item added

--- app/src/main/java/com/example/vickypatel/multipanelayouttest/MainActivity.java
+++ app/src/main/java/com/example/vickypatel/multipanelayouttest/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.vickypatel.multipanelayouttest;
 
+import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -13,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
     RecyclerView mRecyclerView;
     RecyclerViewAdapter mRecyclerViewAdapter;
     boolean mTwoPane = false;
+    ArrayList<String> data = new ArrayList<>(Arrays.asList("Xyz", "Abc", "Pqr"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +23,13 @@ public class MainActivity extends AppCompatActivity {
 
         if (findViewById(R.id.item_detail_container) != null) {
             mTwoPane = true;
+            Bundle arguments = new Bundle();
+            arguments.putString(DetailFragment.DATA, data.get(0));
+            DetailFragment fragment = new DetailFragment();
+            fragment.setArguments(arguments);
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.item_detail_container, fragment)
+                    .commit();
         }
 
 
@@ -29,7 +38,7 @@ public class MainActivity extends AppCompatActivity {
 
         mRecyclerViewAdapter = new RecyclerViewAdapter(
                 this,
-                new ArrayList<>(Arrays.asList("Xyz", "Abc", "Pqr")),
+                data,
                 mTwoPane);
 
         mRecyclerView.setAdapter(mRecyclerViewAdapter);
